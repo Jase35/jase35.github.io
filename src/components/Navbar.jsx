67f94415f9746c3282183a54c343abd18162751c
Beaubien, Jase A <jasebeaubien@gmail.com>
2025-09-08 10:41:55 -0500

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,71 +5,60 @@ import infoData from '../assets/info.json';
 function Navbar() {
   return (
     <div className="fixed z-50 shadow-md flex flex-col xl:flex-row xl:justify-between absolute bg-main w-full right-0 left-0 top-0 xl:pl-2">
-        <div>
-            <NavLink to="/" className="head-text-theme text-lg xl:text-4xl text-center">Jase Beaubien</NavLink>
-        </div>
-        <nav className="justify-center items-center w-full xl:w-auto">
-            <ul style={{ listStyle: 'none', display: 'flex', gap: '0px' }} className="justify-between xl:justify-center xl:items-center">
-                <li className="relative group m-0 !p-0 h-auto w-1/3 xl:w-auto navlink">
-                    <NavLink
-                        className="navlink-text !w-full text-sm xl:text-4xl"
-                        to="/"
-                    >
-                        Home
-                    </NavLink>
+        <NavLink to="/" className="xl:p-2 head-text-theme text-lg xl:text-4xl text-center">Jase Beaubien</NavLink>
+        <nav className="flex justify-between xl:justify-center items-center w-full xl:w-auto">
+            <NavLink
+                className="xl:p-2 xl:pl-4 xl:pr-4 relative group navlink w-1/3 xl:w-auto text-sm xl:text-4xl"
+                to="/"
+            >
+                Home
+                <ul className="absolute z-49 hidden xl:group-hover:block bg-theme-alt left-0 right-0 mt-2">
+                    <li>
+                        <HashLink to="/#about" className="subnavlink">About</HashLink>
+                    </li>
+                    <li>
+                        <HashLink to="/#mission" className="subnavlink">Mission</HashLink>
+                    </li>
+                    <li>
+                        <HashLink to="/#answers" className="subnavlink">Q & A</HashLink>
+                    </li>
+                </ul>
+            </NavLink>
 
-                    <ul className="absolute z-49 hidden xl:group-hover:block bg-main left-0 right-0">
-                        <li>
-                            <HashLink to="/#about" className="subnavlink">About</HashLink>
-                        </li>
-                        <li>
-                            <HashLink to="/#mission" className="subnavlink">Mission</HashLink>
-                        </li>
-                        <li>
-                            <HashLink to="/#answers" className="subnavlink">Q & A</HashLink>
-                        </li>
-                    </ul>
-                </li>
-                <li className="relative group w-1/3 xl:w-auto navlink">
-                    <NavLink
-                        className="navlink-text text-sm xl:text-4xl"
-                        to="/experience"
-                    >
-                        Experience
-                    </NavLink>
+            <NavLink
+                className="xl:p-2 xl:pl-4 xl:pr-4 relative group navlink w-1/3 xl:w-auto text-sm xl:text-4xl"
+                to="/experience"
+            >
+                Experience
+                <ul className="absolute z-49 hidden xl:group-hover:block bg-main left-0 right-0 mt-2">
+                    <li>
+                        <HashLink to="/experience#projects" className="subnavlink">Projects</HashLink>
+                    </li>
+                    <li>
+                        <HashLink to="/experience#education" className="subnavlink">Education</HashLink>
+                    </li>
+                    <li>
+                        <HashLink to="/experience#employment" className="subnavlink">Employment</HashLink>
+                    </li>
+                    <li>
+                        <HashLink to="/experience#extracurriculars" className="subnavlink">Extracurriculars</HashLink>
+                    </li>
+                </ul>
+            </NavLink>
 
-                    <ul className="absolute z-49 hidden xl:group-hover:block bg-main left-0 right-0">
-                        <li>
-                            <HashLink to="/experience#projects" className="subnavlink">Projects</HashLink>
-                        </li>
-                        <li>
-                            <HashLink to="/experience#education" className="subnavlink">Education</HashLink>
-                        </li>
-                        <li>
-                            <HashLink to="/experience#employment" className="subnavlink">Employment</HashLink>
-                        </li>
-                        <li>
-                            <HashLink to="/experience#extracurriculars" className="subnavlink">Extracurriculars</HashLink>
-                        </li>
-                    </ul>
-                </li>
-                <li className="relative group w-1/3 xl:w-auto navlink">
-                    <NavLink
-                        className="navlink-text text-sm xl:text-4xl"
-                        to="/contact"
-                    >
-                        Contact
-                    </NavLink>
-
-                    <ul className="absolute z-49 hidden xl:group-hover:block bg-main left-0 right-0">
-                        <li>
-                            <button className="subnavlink" onClick={() => window.open('mailto:' + infoData.email, '_blank', 'noopener,noreferrer')}>
-                                Email Now
-                            </button>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+            <NavLink
+                className="xl:p-2 xl:pl-4 xl:pr-4 relative group navlink w-1/3 xl:w-auto text-sm xl:text-4xl"
+                to="/contact"
+            >
+                Contact
+                <ul className="absolute z-49 hidden xl:group-hover:block bg-main left-0 right-0 mt-2">
+                    <li>
+                        <button className="subnavlink" onClick={() => window.open('mailto:' + infoData.email, '_blank', 'noopener,noreferrer')}>
+                            Email Now
+                        </button>
+                    </li>
+                </ul>
+            </NavLink>
         </nav>
     </div>
   );
