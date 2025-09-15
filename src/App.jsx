8fcbba37a38a529adf4804199d63c6b8b6e900d1
Beaubien, Jase A <jasebeaubien@gmail.com>
2025-09-15 10:57:49 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,13 @@ function App() {
  return (
     <div>
       <Navbar />
-      <div className="absolute top-14 xl:top-18 left-2 right-2 xl:left-50 xl:right-50">
-        <HashRouter>
+        <div className="absolute top-14 xl:top-18 left-2 right-2 xl:left-50 xl:right-50">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/experience" element={<Projects />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/project/:id" element={<ProjectInfo />} />
           </Routes>
-        </HashRouter>
         <div className="mb-20" />
       </div>
     </div>
