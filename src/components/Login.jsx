208d61d2e4a97aa294ef56c582d3db42f12a00f7
Beaubien, Jase A <jasebeaubien@gmail.com>
2026-01-31 14:49:22 -0600

    game

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Login({ onLogin }) {
     const [username, setUsername] = useState('');
     const [imageUrl, setImageUrl] = useState('');
+
+    const attemptLog = async () => {
+        try {
+            const response = await fetch('http://localhost:4000/user/self', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            });
+            if (response.ok) {
+                const user = await response.json();
+                onLogin(user);
+                alert('Session restored.');
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert('An error occurred: ' + error);
+        }
+    }
 
     const handleSubmit = async () => {
         try {
@@ -19,15 +38,18 @@ function Login({ onLogin }) {
             if (response.ok) {
                 const user = await response.json();
                 onLogin(user);
-                alert('User created successfully!');
             } else {
                 alert('Failed to create user.');
             }
         } catch (error) {
             console.error('Error:', error);
-            alert('An error occurred.');
+            alert('An error occurred: ' + error);
         }
     };
+
+    useEffect(() => {
+        attemptLog();
+    }, []);
 
     return (
         <div>
